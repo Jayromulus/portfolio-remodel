80e20a16ac52c9f8c9ec9dc20843e3ef8ee4d0c5
unknown <jerbear2@gmail.com>
2019-09-10 00:01:37 -0400

    Updates

---
+++ precache-manifest.cf9043a1a8b3142449c6c5201bf7f20c.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9a9a5fd05457cf2ff41e2f2fe1d399c",
+    "url": "/Jayromulus/portfolio-remodel/index.html"
+  },
+  {
+    "revision": "c0d2625601909d40c45e",
+    "url": "/Jayromulus/portfolio-remodel/static/css/main.9abccb4f.chunk.css"
+  },
+  {
+    "revision": "dd74272f2e49f9322eb4",
+    "url": "/Jayromulus/portfolio-remodel/static/js/2.60e3d9f6.chunk.js"
+  },
+  {
+    "revision": "c0d2625601909d40c45e",
+    "url": "/Jayromulus/portfolio-remodel/static/js/main.5e9b47a7.chunk.js"
+  },
+  {
+    "revision": "3f22972f16a80b1ce18c",
+    "url": "/Jayromulus/portfolio-remodel/static/js/runtime~main.64c1c80c.js"
+  },
+  {
+    "revision": "0040d92a7f81ec58b7480be97fd78dce",
+    "url": "/Jayromulus/portfolio-remodel/static/media/Angular-logo2.0040d92a.png"
+  },
+  {
+    "revision": "04cf122be0afe7542083d1a3967390ae",
+    "url": "/Jayromulus/portfolio-remodel/static/media/CSS3-logo.04cf122b.png"
+  },
+  {
+    "revision": "9cf4a14cb5024b5d6e988c1519570ce3",
+    "url": "/Jayromulus/portfolio-remodel/static/media/HTML5-logo.9cf4a14c.png"
+  },
+  {
+    "revision": "0cded3a3276425911d55a2552bf361bf",
+    "url": "/Jayromulus/portfolio-remodel/static/media/JavaScript-logo.0cded3a3.png"
+  },
+  {
+    "revision": "7ee6dd38ba856349b2805b5dfc59b453",
+    "url": "/Jayromulus/portfolio-remodel/static/media/MashBG.7ee6dd38.png"
+  },
+  {
+    "revision": "167c6815dcc4f38bcfc99876305b4f34",
+    "url": "/Jayromulus/portfolio-remodel/static/media/React-logo.167c6815.png"
+  },
+  {
+    "revision": "ac063a13241c1e823d91ccfb3cf39c13",
+    "url": "/Jayromulus/portfolio-remodel/static/media/apiBG.ac063a13.png"
+  },
+  {
+    "revision": "5c4afe567936be322140567443d622b0",
+    "url": "/Jayromulus/portfolio-remodel/static/media/phoneANGULARBG.5c4afe56.png"
+  },
+  {
+    "revision": "34b04a730f80661170e8c8b27a030072",
+    "url": "/Jayromulus/portfolio-remodel/static/media/phoneCSSBG.34b04a73.png"
+  },
+  {
+    "revision": "ae1e8bfd07bb6431c2c1cfc4d628f67a",
+    "url": "/Jayromulus/portfolio-remodel/static/media/phoneHTMLBG.ae1e8bfd.png"
+  },
+  {
+    "revision": "0864fbaa04d2f870de7e1bea61fe06d7",
+    "url": "/Jayromulus/portfolio-remodel/static/media/phoneREACTBG.0864fbaa.png"
+  }
+]);
