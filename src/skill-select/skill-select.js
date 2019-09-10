a62fd39f27bf8385caf16ab057e726c729606b1f
unknown <jerbear2@gmail.com>
2019-09-10 00:00:19 -0400

    trying to deploy

--- src/skill-select/skill-select.js
+++ src/skill-select/skill-select.js
@@ -24,69 +24,70 @@ function Skills() {
     let [current, setCurrent] = useState('css');
 
     return (
-        <div 
-            id="css-width" 
-            className={
-                current === 'css' ? 'cssbg' :
-                current === 'react' ? 'reactbg' : 
-                current === 'angular' ? 'angularbg' : 
-                current === 'html' ? 'htmlbg' : null
-            }>
+        <div
+            id="css-width"
+        // className={
+        //     current === 'css' ? 'cssbg' :
+        //     current === 'react' ? 'reactbg' : 
+        //     current === 'angular' ? 'angularbg' : 
+        //     current === 'html' ? 'htmlbg' : null
+        // }
+        >
             <ButtonGroup>
                 <div id="top-row">
-                    <Button 
-                        className={current === 'html' ? "active" : "inactive"} 
-                        onClick={() => {setCurrent('html');}} 
+                    <Button
+                        className={current === 'html' ? "active" : "inactive"}
+                        onClick={() => { setCurrent('html'); }}
                         id="HTML"><br />
                     </Button>
-                    <Button 
-                        className={current === 'css' ? "active" : "inactive"} 
-                        onClick={() => {setCurrent('css');}} 
+                    <Button
+                        className={current === 'css' ? "active" : "inactive"}
+                        onClick={() => { setCurrent('css'); }}
                         id="CSS"><br />
                     </Button>
-                    <Button 
-                        className={current === 'js' ? "active" : "inactive"} 
-                        onClick={() => {setCurrent('js');}} 
+                    <Button
+                        className={current === 'js' ? "active" : "inactive"}
+                        onClick={() => { setCurrent('js'); }}
                         id="JS"><br />
                     </Button>
                 </div>
                 <br />
                 <div id="middle-row">
-                    <Button 
-                        className={current === 'react' ? "active" : "inactive"} 
-                        onClick={() => {setCurrent('react');}} 
+                    <Button
+                        className={current === 'react' ? "active" : "inactive"}
+                        onClick={() => { setCurrent('react'); }}
                         id="React"><br />
                     </Button>
-                    <Button 
-                        className={current === 'me' ? "active" : "inactive"} 
-                        onClick={() => {setCurrent('me');}} 
+                    <Button
+                        className={current === 'me' ? "active" : "inactive"}
+                        onClick={() => { setCurrent('me'); }}
                         id="jerome">Me
                     </Button>
-                    <Button 
-                        className={current === 'angular' ? "active" : "inactive"} 
-                        onClick={() => {setCurrent('angular');}} 
+                    <Button
+                        className={current === 'angular' ? "active" : "inactive"}
+                        onClick={() => { setCurrent('angular'); }}
                         id="Angular"><br />
                     </Button>
                 </div>
                 <br />
                 <div id="bottom-row">
                     {/* <a href="https://jeromeapi.firebaseapp.com/" target="blank"> */}
-                        <Button 
-                            className={current === 'api' ? "active" : "inactive"} 
-                            onClick={() => {setCurrent('api');}} 
-                            id="API"><br />
-                        </Button>
+                    <Button
+                        className={current === 'api' ? "active" : "inactive"}
+                        onClick={() => { setCurrent('api'); }}
+                        id="API"><br />
+                    </Button>
                     {/* </a> */}
                     {/* <a href="https://jf-twit-combo.herokuapp.com/" target="blank"> */}
-                        <Button
-                            className={current === 'blue' ? "active" : "inactive"} 
-                            onClick={() => {setCurrent('blue');}} 
-                            id="Mashr"><br />
-                        </Button>
+                    <Button
+                        className={current === 'blue' ? "active" : "inactive"}
+                        onClick={() => { setCurrent('blue'); }}
+                        id="Mashr"><br />
+                    </Button>
                     {/* </a> */}
-                    <Button 
-                        className={current === 'wip' ? "active" : "inactive"} 
-                        onClick={() => {setCurrent('wip');}}>WIP
+                    <Button
+                        className={current === 'wip' ? "active" : "inactive"}
+                        onClick={() => { setCurrent('wip'); }}>WIP
                     </Button>
                 </div>
             </ButtonGroup>
